bd4309f416fb2bd0542bda1d256f9b0e5a1b8ff7
Jeedie JideX <enockyona906@gmail.com>
2023-03-12 04:09:19 +0300

    add react router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./user/components/navbar/Navbar";
 
@@ -6,6 +7,11 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <h1>Jeedie React Dev</h1>
+      <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
