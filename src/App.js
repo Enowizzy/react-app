be4109059ecf8ccc0ca48cbf8d8fce737b1c3ce3
Jeedie JideX <enockyona906@gmail.com>
2023-03-12 04:01:38 +0300

    navbar implementstion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./user/components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Jeedie React Dev</h1>
     </div>
   );
