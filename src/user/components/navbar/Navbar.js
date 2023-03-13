40255c6742a78ec8ef25d134ad1c6ab42263624c
Jeedie JideX <enockyona906@gmail.com>
2023-03-13 17:49:14 +0300

    navbar implement

--- src/user/components/navbar/Navbar.js
+++ src/user/components/navbar/Navbar.js
@@ -11,15 +11,7 @@ const Navbar = () => {
           <Link to="/news">News</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/about" class="active">About</Link>
-          <button
-            className="nav-btn nav-close-btn"
-          >
-          </button>
         </nav>
-        <button
-          className="nav-btn"
-        >
-        </button>
       </header>
     </div>
   );
