cb0ccf2b5502e95ca647af075ec73b85a56a4ead
Jeedie JideX <enockyona906@gmail.com>
2023-03-13 17:58:09 +0300

    react icons install

--- src/user/components/navbar/Navbar.js
+++ src/user/components/navbar/Navbar.js
@@ -1,17 +1,36 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle(
+      "responsive_nav"
+    );
+  };
   return (
     <div>
       <header>
         <h3>LOGO</h3>
-        <nav>
+        <nav ref={navRef}>
           <Link to="/">Home</Link>
           <Link to="/news">News</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/about" class="active">About</Link>
+          <button
+            className="nav-btn nav-close-btn"
+            onClick={showNavbar}>
+            <FaTimes />
+          </button>
         </nav>
+        <button
+          className="nav-btn"
+          onClick={showNavbar}>
+          <FaBars />
+        </button>
       </header>
     </div>
   );
