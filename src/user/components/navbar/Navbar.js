9e84fccba3d0ab19e85352a381a9d07020708c9b
Jeedie JideX <enockyona906@gmail.com>
2023-03-13 17:40:08 +0300

    navbar implement

--- src/user/components/navbar/Navbar.js
+++ src/user/components/navbar/Navbar.js
@@ -1,8 +1,26 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
+      <header>
+			<h3>LOGO</h3>
+			<nav>
+				<a href="/#">Home</a>
+				<a href="/#">My work</a>
+				<a href="/#">Blog</a>
+				<a href="/#">About me</a>
+				<button
+					className="nav-btn nav-close-btn"
+					>
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				>
+			</button>
+		</header>
       <ul>
         <li>
           <Link to="/">Home</Link>
