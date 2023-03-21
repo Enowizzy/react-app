88f29847fedc61cc24f88faf898b9a1a9fed317f
Jeedie JideX <enockyona906@gmail.com>
2023-03-21 12:02:09 +0300

    admin edit-news template

--- src/admin/components/add-news/addNews.js
+++ src/admin/components/add-news/addNews.js
@@ -1,9 +1,30 @@
 const AddNews = () => {
-    return ( 
-        <div class="container">
-
+    return (
+        <div class="container mt-3">
+            <div class="card text-center">
+                <div class="card-header">
+                    <ul class="nav nav-pills card-header-pills">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">All News</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div className="card-footer">
+                    <a href="#" class="btn btn-primary">Add News</a>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default AddNews;
