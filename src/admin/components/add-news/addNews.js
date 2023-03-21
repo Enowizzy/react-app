925891706ca48ca5c60d2b4b267d6120ed081f88
Jeedie JideX <enockyona906@gmail.com>
2023-03-21 13:53:29 +0300

    admin add news form

--- src/admin/components/add-news/addNews.js
+++ src/admin/components/add-news/addNews.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const AddNews = () => {
+    const [author_first_name, first_name_change] = useState('');
+    const [author_last_name, last_name_change] = useState('');
+    const [news_message, news_change] = useState('');
+
+    const addNews = (e) => {
+        e.preventDefault();
+        const news_obj = { author_first_name, author_last_name, news_message };
+        console.log(news_obj);
+    }
     return (
         <div class="container col-md-8 mt-5">
             <div class="card text-center">
@@ -18,24 +29,24 @@ const AddNews = () => {
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">News Form</h5>
-                    <form class="text-center">
+                    <form class="text-center" onSubmit={addNews}>
                         <div class="row">
                             <div class="col">
-                                <input type="text" class="form-control" placeholder="Author first name"></input>
+                                <input type="text" value={author_first_name} onChange={e => first_name_change(e.target.value)} class="form-control" placeholder="Author first name"></input>
                             </div>
                             <div class="col">
-                                <input type="text" class="form-control" placeholder="Author last name"></input>
+                                <input type="text" value={author_last_name} onChange={e => last_name_change(e.target.value)} class="form-control" placeholder="Author last name"></input>
                             </div>
                         </div>
                         <div class="row mt-3">
                             <div class="form-group">
-                                <textarea class="form-control" id="exampleFormControlTextarea1" rowsF="3" placeholder="News area"></textarea>
+                                <textarea class="form-control" value={news_message} onChange={e => news_change(e.target.value)} id="exampleFormControlTextarea1" rowsF="3" placeholder="News area"></textarea>
                             </div>
                         </div>
                     </form>
                 </div>
                 <div className="card-footer">
-                    <a href="#" class="btn btn-primary float-end">Add News</a>
+                    <a href="#" type="submit" class="btn btn-primary float-end">Add News</a>
                 </div>
             </div>
         </div>
