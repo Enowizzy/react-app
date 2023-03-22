901b6c82f83e29521b9e0154dc6615fefb390fb3
Jeedie JideX <enockyona906@gmail.com>
2023-03-22 12:41:10 +0300

    news page commit

--- src/user/components/news/news.js
+++ src/user/components/news/news.js
@@ -1,21 +1,46 @@
+import { useEffect, useState } from "react";
+
 const News = () => {
-    let news_lists$ = [
-        { id: 1, name: "React", tech: "jsx" },
-        { id: 2, name: "Angular", tech: "typescript" },
-        { id: 3, name: "VueJs", tech: "html" },
-    ];
+    const [news_lists$, news_update$] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/news').then(res => {
+            return res.json();
+        }).then(resp => {
+            news_update$(resp);
+        }).catch((err) => {
+            console.log(err.message)
+        });
+    }, [])
     return (
-        <div>
+        <div class="container col-md-10 justify-content-center mt-3">
             <h2>News Page</h2>
-            {
-                news_lists$.map((news_list$) => (
-                    <div key={news_list$.id}>
-                        <h3>ID is: {news_list$.id}</h3>
-                        <h3>Name is: {news_list$.name}</h3>
-                        <h3>Technology: {news_list$.tech}</h3>
-                    </div>
-                ))
-            }
+            <table className="table table-bordered">
+                <thead className="bg-dark text-white">
+                    <tr>
+                        <td>Id</td>
+                        <td>Title</td>
+                        <td>Author</td>
+                        <td>Action</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        news_lists$.map((news_list$) => (
+                            <tr key={news_list$.id}>
+                                <td>{news_list$.id}</td>
+                                <td>{news_list$.author_first_name}</td>
+                                <td>{news_list$.author_last_name}</td>
+                                <td>
+                                    <a href="view" className="btn btn-primary">View</a>
+                                    <a href="edit" className="btn btn-warning">Edit</a>
+                                    <a href="delete" className="btn btn-danger">Delete</a>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
