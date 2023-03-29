9f1b829f1af5990faff772a083d44c041f671855
Jeedie JideX <enockyona906@gmail.com>
2023-03-29 08:20:03 +0300

    add user-list to template

--- src/admin/components/users/UsersList.js
+++ src/admin/components/users/UsersList.js
@@ -41,6 +41,7 @@ const UsersList = () => {
                         <td>Phone</td>
                         <td>Email</td>
                         <td>Role</td>
+                        <td>Actions</td>
                     </tr>
                 </thead>
                 <tbody>
