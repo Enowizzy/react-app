14cde9a8466105423fd91c86172aa6d5fb1444e9
Jeedie JideX <enockyona906@gmail.com>
2023-03-30 14:37:56 +0300

    news template

--- src/admin/components/dashboard/Dashboard.js
+++ src/admin/components/dashboard/Dashboard.js
@@ -91,8 +91,8 @@ function Dashboard() {
                                 data-parent="#accordionSidebar">
                                 <div className="bg-white py-2 collapse-inner rounded">
                                     <h6 className="collapse-header">News Utilities:</h6>
-                                    <Link className="collapse-item" href="utilities-color.html">Create</Link>
-                                    <Link className="collapse-item" href="utilities-border.html">View</Link>
+                                    <Link className="collapse-item" to="/admin/create-news">Create</Link>
+                                    <Link className="collapse-item" to="/news">View</Link>
                                     <a className="collapse-item" href="utilities-animation.html">Animations</a>
                                     <a className="collapse-item" href="utilities-other.html">Other</a>
                                 </div>
