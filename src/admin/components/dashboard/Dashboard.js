f2d4eda51bdc72a9ff9af9fcae7bada0baad3cbf
Jeedie JideX <enockyona906@gmail.com>
2023-03-30 14:34:38 +0300

    news template

--- src/admin/components/dashboard/Dashboard.js
+++ src/admin/components/dashboard/Dashboard.js
@@ -85,14 +85,14 @@ function Dashboard() {
                             <a className="nav-link collapsed" href="/" data-toggle="collapse" data-target="#collapseUtilities"
                                 aria-expanded="true" aria-controls="collapseUtilities">
                                 <i className="fas fa-fw fa-wrench"></i>
-                                <span>Utilities</span>
+                                <span>News</span>
                             </a>
                             <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
                                 data-parent="#accordionSidebar">
                                 <div className="bg-white py-2 collapse-inner rounded">
-                                    <h6 className="collapse-header">Custom Utilities:</h6>
-                                    <a className="collapse-item" href="utilities-color.html">Colors</a>
-                                    <a className="collapse-item" href="utilities-border.html">Borders</a>
+                                    <h6 className="collapse-header">News Utilities:</h6>
+                                    <a className="collapse-item" href="utilities-color.html">Create</a>
+                                    <a className="collapse-item" href="utilities-border.html">View</a>
                                     <a className="collapse-item" href="utilities-animation.html">Animations</a>
                                     <a className="collapse-item" href="utilities-other.html">Other</a>
                                 </div>
